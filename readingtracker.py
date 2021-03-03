30666550e4f903bdde49bfbf68eae830875561c2
georgemortleman27 <79975078+georgemortleman27@users.noreply.github.com>
2021-03-03 10:58:29 +0000

    Add files via upload

---
+++ readingtracker.py
@@ -0,0 +1,13 @@
+book_list = open("booklist.txt", "a")
+choice = input("add new or see read ").lower()
+if choice == "add new":
+	book_name = input("book name: ")
+	author_name = input("author name: ")
+	book_rating = input("rate the book: ")
+	book_list.write(f"{book_name}, {author_name}, {book_rating}" + "\n")
+	print(f"{book_name} has been added to your booklist")
+elif choice == "see read":
+	book_list_readable = open("booklist.txt", "r")
+	output = book_list_readable.read()
+	print(output)
+	
